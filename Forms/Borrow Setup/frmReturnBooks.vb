1e618ce0555b4bc857b82e13fc4ba217b5287e84
Clifford Villena <cliffordvillenaaa@gmail.com>
2024-04-29 14:33:25 +0800

    first commit

---
+++ Forms/Borrow Setup/frmReturnBooks.vb
@@ -0,0 +1,74 @@
+﻿Public Class frmReturnBooks
+
+    Dim getBorrowID As Integer = Nothing
+
+    Public Sub SetSelectedBorrowedBooks(borrowID As Integer, studentID As Integer, firstname As String, lastname As String, isbn As String, author As String, accessionNo As String, title As String)
+        getBorrowID = borrowID
+        txtStudentID.Text = studentID
+        txtFirstname.Text = firstname
+        txtLastname.Text = lastname
+        txtISBN.Text = isbn
+        txtAuthors.Text = author
+        txtAcn.Text = accessionNo
+        txtTitle.Text = title
+    End Sub
+
+    Private Sub frmReturnBooks_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtStatus.Items.Remove("Overdue")
+        If IsBookOverdue(getBorrowID) Then
+            CalculateInOverdue(getBorrowID, txtPenalty, txtStatus)
+            txtStatus.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim copyID As Integer = GetCopyIDFunction(txtAcn.Text)
+
+        Try
+            If txtStatus.SelectedIndex = 0 Then
+                'good condition
+                ReturnBookInGood(getBorrowID, copyID, txtStudentID.Text)
+            ElseIf txtStatus.SelectedIndex = 1 Then
+                'overdue condition
+                ReturnOverdue(getBorrowID, txtPenalty.Text)
+            ElseIf txtStatus.SelectedIndex = 2 Then
+                'damaged condition
+                ReturnDamaged(getBorrowID, txtPenalty.Text)
+            ElseIf txtStatus.SelectedIndex = 3 Then
+            End If
+
+            getBorrowID = Nothing
+            Me.Close()
+        Catch ex As Exception
+            MessageBox.Show($"Error: {ex.Message}")
+        End Try
+    End Sub
+
+    Private Sub txtStatus_SelectedIndexChanged(sender As Object, e As EventArgs) Handles txtStatus.SelectedIndexChanged
+        If txtStatus.SelectedIndex = 1 Then
+            Dim type As String = GetBookType(txtAcn.Text)
+            If type = "Purchased" Then
+                txtPenalty.ReadOnly = True
+                txtPenalty.Text = GetBookPrice(txtAcn.Text)
+            ElseIf type = "Donated" Then
+                txtPenalty.Text = GetBookPenalty()
+                txtPenalty.ReadOnly = True
+            End If
+
+        ElseIf txtStatus.SelectedIndex = 0 Then
+            txtPenalty.Clear()
+            txtPenalty.ReadOnly = False
+
+        ElseIf txtStatus.SelectedIndex = 2 Then
+            Dim type As String = GetBookType(txtAcn.Text)
+            If type = "Purchased" Then
+                txtPenalty.Text = GetBookPrice(txtAcn.Text)
+                txtPenalty.ReadOnly = True
+            ElseIf type = "Donated" Then
+                txtPenalty.Text = GetBookPenalty()
+                txtPenalty.ReadOnly = True
+            End If
+
+        End If
+    End Sub
+End Class
---
+++ Forms/Others/frmReports.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Drawing.Printing
+
+Public Class frmReports
+
+    Private printDocument As New PrintDocument()
+    Private printPreviewDialog As New PrintPreviewDialog()
+
+    Private Sub frmReports_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim dtReports As DataTable = DisplayReport()
+        dgReports.DataSource = dtReports
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        PrintReport()
+    End Sub
+    Private Sub PrintDocument_PrintPage(ByVal sender As Object, ByVal e As Printing.PrintPageEventArgs)
+        Dim printArea As RectangleF = e.MarginBounds
+
+        Dim font As New Font("Arial", 10)
+        Dim brush As New SolidBrush(Color.Black)
+
+        Dim xPos As Single = printArea.Left
+        Dim yPos As Single = printArea.Top
+
+        Dim rowHeight As Single = font.GetHeight() + 5
+
+        Dim dt As DataTable = DisplayReport()
+
+        ' Define column widths and alignment
+        Dim columnWidths As New List(Of Single)({110, 100, 100, 100, 140, 140, 100}) ' Adjust widths as needed
+        Dim columnAlignments As New List(Of String)({"Left", "Left", "Left", "Left", "Left", "Left", "Right"})
+        Dim columnHeaders As New List(Of String)({"ISBN", "Book Title", "Authors", "Fullname", "Date Borrowed", "Date Returned", "Penalty"})
+
+        ' Display column headers
+        For i As Integer = 0 To columnHeaders.Count - 1
+            Dim columnText As String = columnHeaders(i)
+            Dim alignment As String = columnAlignments(i)
+            Dim columnWidth As Single = columnWidths(i)
+            If alignment = "Right" Then
+                xPos += columnWidth - e.Graphics.MeasureString(columnText, font).Width
+            End If
+            e.Graphics.DrawString(columnText, font, brush, xPos, yPos)
+            If alignment = "Center" OrElse alignment = "Right" Then
+                xPos -= columnWidth - e.Graphics.MeasureString(columnText, font).Width
+            End If
+            xPos += columnWidth + 10
+        Next
+
+        yPos += rowHeight
+
+        ' Display rows
+        For Each row As DataRow In dt.Rows
+            xPos = printArea.Left
+            For i As Integer = 0 To dt.Columns.Count - 1
+                Dim columnText As String = row(i).ToString()
+                Dim alignment As String = columnAlignments(i)
+                Dim columnWidth As Single = columnWidths(i)
+                If alignment = "Right" Then
+                    xPos += columnWidth - e.Graphics.MeasureString(columnText, font).Width
+                End If
+                e.Graphics.DrawString(columnText, font, brush, xPos, yPos)
+                If alignment = "Center" OrElse alignment = "Right" Then
+                    xPos -= columnWidth - e.Graphics.MeasureString(columnText, font).Width
+                End If
+                xPos += columnWidth + 10
+            Next
+            yPos += rowHeight
+        Next
+        e.HasMorePages = False
+    End Sub
+
+
+
+
+
+
+    Private Sub PrintReport()
+        ' Set landscape orientation
+        printDocument.DefaultPageSettings.Landscape = True
+
+        ' Attach event handler
+        AddHandler printDocument.PrintPage, AddressOf PrintDocument_PrintPage
+
+        ' Show print preview dialog
+        printPreviewDialog.Document = printDocument
+        printPreviewDialog.ShowDialog()
+    End Sub
+
+End Class
